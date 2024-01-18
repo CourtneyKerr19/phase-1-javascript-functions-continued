36abba1f6dcc3d4eb1f9faadd286d383209a897a
Courtney Kerr <courtneykerr219@gmail.com>
2024-01-17 22:36:41 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return "This Saturday, I want to " + activity + "!"; 
+}
+
+function mondayWork(activity = "go to the office") {
+    return "This Monday, I will " + activity + ".";
+} 
+
+// Function that returns a function to wrap an adjective in a highlight
+function wrapAdjective(highlight = "*") {
+    return function(adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    }
+  }
+  
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+  };
